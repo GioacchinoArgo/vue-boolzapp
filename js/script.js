4095c6e233e88b42accb8c350b4cc759a021c658
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-26 15:42:00 +0100

    completed milestone 2

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
-console.log('Vue OK', Vue);
+    console.log('Vue OK', Vue);
 
-const app = Vue.createApp ({
-    data() {
-        return {
-            user,
-            contacts,
-            currentId: 1
-        };
-    },
-    methods: {
-        currentContact() {
-            contacts.forEach((contact) => {
-                if (contact.id === currentId) {
-                    return contact.id
-                }
-            });
-        }     
-    }
-});
-app.mount('#root');
+    const app = Vue.createApp ({
+        data() {
+            return {
+                user,
+                contacts,
+                currentId: 1
+            };
+        },
+        computed: {
+            currentContact() {
+                return this.contacts.find(contact => contact.id === this.currentId);
+            },
+            currentChat() {
+                return this.currentContact.messages;
+            }
+        },
+        methods: {
+            setCurrentId(id) {
+                this.currentId = id
+            }
+        }
+    });
+    app.mount('#root');
 
 
 
